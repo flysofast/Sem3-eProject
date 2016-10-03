f260d0d8fbebc2c30d1e523af5a9fcc06b37a60e
Bao Nguyen <baonguyenit89@gmail.com>
2016-10-03 17:52:00 +0700

    Init

---
+++ AirlineReservation/AirlineReservation/Scripts/AirReservation/AirReservationHome.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    //Set default hide return date
+    $("#returnDateForm").hide();
+    $("#lblReturnClass").hide();
+    $("#returnClass").hide();
+    //Check ticket type, show/hide input return date 
+    $("input[name=optFlight]:radio").change(function () {
+        if ($('#optFlightReturn').is(':checked')) {
+            $("#returnDateForm").show();
+            $("#lblReturnClass").show();
+            $("#returnClass").show();
+        } else {
+            $("#returnDateForm").hide();
+            $("#lblReturnClass").hide();
+            $("#returnClass").hide();
+        }
+    });
+});
+
+function swapLocation() {
+    var from = $("#fromLocation").val();
+    var to = $("#toLocation").val();
+    if (from == "" || to == "") {
+        alert("Please select from and to location");
+        return false;
+    }
+    $("#fromLocation").val(to);
+    $("#toLocation").val(from);
+}
