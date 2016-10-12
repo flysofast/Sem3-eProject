cf5cf6114f1ef5c274b6afab73ff7be12876f24a
Bao Nguyen <baonguyenit89@gmail.com>
2016-10-12 15:12:04 +0700

    Login/ Register/Recovery

--- AirlineReservation/AirlineReservation/Scripts/AirReservation/AirReservationHome.js
+++ AirlineReservation/AirlineReservation/Scripts/AirReservation/AirReservationHome.js
@@ -7,15 +7,18 @@
     //Check ticket type, show/hide input return date 
     $("input[name=optFlight]:radio").change(function () {
         if ($('#optFlightReturn').is(':checked')) {
-            $("#returnDateForm").show();
-            $("#lblReturnClass").show();
-            $("#returnClass").show();
+            $("#returnDateForm").show("slow");
+            $("#lblReturnClass").show("slow");
+            $("#returnClass").show("slow");
         } else {
             $("#returnDateForm").hide();
             $("#lblReturnClass").hide();
             $("#returnClass").hide();
         }
     });
+    $("#aboutUsPage").hide();
+    $("#registerFormMain").hide();
+    $("#forgotPasswordFormMain").hide();
 });
 
 function swapLocation() {
@@ -31,4 +34,34 @@ function swapLocation() {
 
 function searchStep1() {
     $("#step1-result").show("slow");
+}
+
+function formMain(mode) {
+    switch (mode) {
+        case "login":
+            $("#registerFormMain").hide("fast");
+            $("#forgotPasswordFormMain").hide("fast");
+            $("#loginFormMain").show("fast");
+            break;
+        case "register":
+            $("#registerFormMain").show("fast");
+            $("#loginFormMain").hide("fast");
+            $("#forgotPasswordFormMain").hide("fast");
+            break;
+        case "forgotPassword":
+            $("#registerFormMain").hide("fast");
+            $("#forgotPasswordFormMain").show("fast");
+            $("#loginFormMain").hide("fast");
+            break;
+        default:
+            $("#registerFormMain").hide("fast");
+            $("#forgotPasswordFormMain").hide("fast");
+            $("#loginFormMain").show("fast");
+            break;
+    }
+}
+
+function aboutUs() {
+    $("#aboutUsPage").slideDown("slow");
+    
 }
